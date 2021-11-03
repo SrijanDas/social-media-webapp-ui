ee8e4ec7aa73b717a6f48ed848ad032994a654eb
Srijan Das <srijandas18.sd@gmail.com>
2021-11-03 15:40:23 +0530

    you can delete posts now

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,23 +1,35 @@
 import "./post.css";
 
+// icons
+import DeleteIcon from "@mui/icons-material/Delete";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import EditIcon from "@mui/icons-material/Edit";
 // firebase imports
-import { ref, getDownloadURL } from "firebase/storage";
+import { ref, getDownloadURL, deleteObject } from "firebase/storage";
 import { storage } from "../../config/firebaseConfig";
 
-import MoreVertIcon from "@material-ui/icons/MoreVert";
 import { useEffect, useState } from "react";
 import axios from "../../axios";
 import { format } from "timeago.js";
 import { Link } from "react-router-dom";
 import DefaultProfilePic from "../../assets/profile.png";
 import LikeIcon from "../../assets/like.png";
-import { Avatar, IconButton, Menu, MenuItem } from "@material-ui/core";
+import {
+  Avatar,
+  IconButton,
+  Menu,
+  MenuItem,
+  ListItemIcon,
+  ListItemText,
+  Alert,
+} from "@mui/material";
 import { useSelector } from "react-redux";
 
 export default function Post({ post }) {
   const [like, setLike] = useState(post.likes.length);
   const [isLiked, setIsLiked] = useState(false);
   const [user, setUser] = useState({});
+  const [postDeleted, setPostDeleted] = useState(false);
 
   const currentUser = useSelector((state) => state.auth.user);
 
@@ -81,9 +93,23 @@ export default function Post({ post }) {
   // post menu actions
   const deletePost = async () => {
     setAnchorEl(null);
+    setPostDeleted(true);
     try {
       await axios.delete("/posts/" + post._id);
-      window.location.reload();
+      if (post.img) {
+        // Create a reference to the file to delete
+        const desertRef = ref(storage, post.img);
+
+        // Delete the file
+        deleteObject(desertRef)
+          .then(() => {
+            // File deleted successfully
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+      // window.location.reload();
     } catch (e) {
       console.log(e);
     }
@@ -102,60 +128,81 @@ export default function Post({ post }) {
   };
 
   return (
-    <div className="post">
-      <div className="postWrapper">
-        <div className="postTop">
-          <div className="postTopLeft">
-            <Link to={`/profile/${user?._id}`}>
-              <Avatar className="postProfileImg" src={profilePic} />
-            </Link>
-            <span className="postUsername">{user?.username}</span>
-            <span className="postDate">{format(post.createdAt)}</span>
-          </div>
-          {currentUser._id === user._id ? (
-            <div className="postTopRight">
-              <IconButton
-                aria-label="more"
-                aria-controls="long-menu"
-                aria-haspopup="true"
-                onClick={openPostMenu}
-              >
-                <MoreVertIcon />
-              </IconButton>
-              <Menu
-                id="long-menu"
-                anchorEl={anchorEl}
-                keepMounted
-                open={open}
-                onClose={closePostMenu}
-              >
-                <MenuItem onClick={deletePost}>Delete</MenuItem>
-              </Menu>
+    <>
+      {postDeleted ? (
+        <Alert style={{ marginBottom: "10px" }} severity="warning">
+          You deleted this post
+        </Alert>
+      ) : (
+        <div className="post">
+          <div className="postWrapper">
+            <div className="postTop">
+              <div className="postTopLeft">
+                <Link to={`/profile/${user?._id}`}>
+                  <Avatar className="postProfileImg" src={profilePic} />
+                </Link>
+                <span className="postUsername">{user?.username}</span>
+                <span className="postDate">{format(post.createdAt)}</span>
+              </div>
+              {currentUser._id === user._id ? (
+                <div className="postTopRight">
+                  <IconButton
+                    aria-label="more"
+                    aria-controls="long-menu"
+                    aria-haspopup="true"
+                    onClick={openPostMenu}
+                  >
+                    <MoreVertIcon />
+                  </IconButton>
+                  <Menu
+                    id="long-menu"
+                    anchorEl={anchorEl}
+                    keepMounted
+                    open={open}
+                    onClose={closePostMenu}
+                  >
+                    <MenuItem>
+                      <ListItemIcon>
+                        <EditIcon fontSize="small" />
+                      </ListItemIcon>
+                      <ListItemText>Edit</ListItemText>
+                    </MenuItem>
+                    <MenuItem onClick={deletePost}>
+                      <ListItemIcon>
+                        <DeleteIcon fontSize="small" />
+                      </ListItemIcon>
+                      <ListItemText>Delete</ListItemText>
+                    </MenuItem>
+                  </Menu>
+                </div>
+              ) : (
+                ""
+              )}
             </div>
-          ) : (
-            ""
-          )}
-        </div>
-        <div className="postCenter">
-          {post.desc ? <span className="postText">{post.desc}</span> : null}
-          {post.img ? <img className="postImg" src={postImg} alt="" /> : null}
-        </div>
-        <div className="postBottom">
-          <div className="postBottomLeft">
-            <img
-              className="likeIcon"
-              src={LikeIcon}
-              onClick={likeHandler}
-              alt=""
-            />
+            <div className="postCenter">
+              {post.desc ? <span className="postText">{post.desc}</span> : null}
+              {post.img ? (
+                <img className="postImg" src={postImg} alt="" />
+              ) : null}
+            </div>
+            <div className="postBottom">
+              <div className="postBottomLeft">
+                <img
+                  className="likeIcon"
+                  src={LikeIcon}
+                  onClick={likeHandler}
+                  alt=""
+                />
 
-            <span className="postLikeCounter">{like} people liked it</span>
-          </div>
-          <div className="postBottomRight">
-            <span className="postCommentText">{post.comment} comments</span>
+                <span className="postLikeCounter">{like} people liked it</span>
+              </div>
+              <div className="postBottomRight">
+                <span className="postCommentText">{post.comment} comments</span>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 }
