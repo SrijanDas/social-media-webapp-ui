b9bb4e9119ef6d2bb910e52611cc4be24a2bc62d
Srijan Das <srijandas18.sd@gmail.com>
2021-11-14 18:14:39 +0530

    added infinite scroll for posts

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -6,12 +6,16 @@ import Post from "../Post/Post";
 import { useSelector } from "react-redux";
 import Loader from "../Loader/Loader";
 import Alert from "@mui/material/Alert";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export default function Feed({ userId }) {
   const [posts, setPosts] = useState([]);
   const currentUser = useSelector((state) => state.auth.user);
   const [isLoading, setIsLoading] = useState(true);
   const [loadingError, setloadingError] = useState(false);
+  const [page, setPage] = useState(2);
+  const [hasMore, setHasMore] = useState(true);
+  const limit = 1;
 
   useEffect(() => {
     // console.log(res.data);
@@ -22,12 +26,12 @@ export default function Feed({ userId }) {
           : await axios.get(`posts/timeline/${currentUser?._id}`);
 
         setPosts(
-          res.data.sort((p1, p2) => {
+          res.data.results.sort((p1, p2) => {
             setIsLoading(false);
             return new Date(p2.createdAt) - new Date(p1.createdAt);
           })
         );
-
+        if (!res.data.next) setHasMore(false);
         setIsLoading(false);
       } catch (error) {
         console.log(error);
@@ -38,6 +42,26 @@ export default function Feed({ userId }) {
     fetchPosts();
   }, [userId, currentUser]);
 
+  const fetchMorePosts = async () => {
+    setPage(page + 1);
+    try {
+      const res = userId
+        ? await axios.get(`posts/profile/${userId}?page=${page}&limit=${limit}`)
+        : await axios.get(
+            `posts/timeline/${currentUser?._id}?page=${page}&limit=${limit}`
+          );
+
+      const newPosts = res.data.results.sort((p1, p2) => {
+        return new Date(p2.createdAt) - new Date(p1.createdAt);
+      });
+      setPosts([...posts, ...newPosts]);
+
+      if (!res.data.next) setHasMore(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="feed">
       <div className="feedWrapper">
@@ -46,7 +70,22 @@ export default function Feed({ userId }) {
         {isLoading ? (
           <Loader size={40} />
         ) : posts.length ? (
-          posts.map((p) => <Post key={p._id} post={p} />)
+          <>
+            <InfiniteScroll
+              dataLength={posts?.length}
+              next={() => {
+                window.setTimeout(() => {
+                  fetchMorePosts();
+                }, 500);
+              }}
+              hasMore={hasMore}
+              loader={<Loader size={40} />}
+            >
+              {posts.map((p, index) => (
+                <Post key={p._id + "-" + index} post={p} />
+              ))}
+            </InfiniteScroll>
+          </>
         ) : loadingError ? (
           <Alert className="feed__msg" severity="error">
             Something went wrong!
